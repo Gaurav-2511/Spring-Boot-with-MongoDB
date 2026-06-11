bdfbc526853eb2a3ca60cde618a8d48997634f4e
Gaurav-2511 <gauravlahamage24@gmail.com>
2026-06-11 19:34:39 +0530

    Added Lombok dependency and refactored code

--- journalApp/src/main/java/com/gaurav/entity/JournalEntry.java
+++ journalApp/src/main/java/com/gaurav/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.gaurav.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,41 +19,4 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime dateTime;
-
-
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-
-
 }
